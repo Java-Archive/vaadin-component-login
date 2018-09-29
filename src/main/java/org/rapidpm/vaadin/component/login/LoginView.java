f104d7bd3f09c59669fdbbb4194d95432c660ad0
Sven Ruppert <sven.ruppert@gmail.com>
2018-09-29 19:38:08 +0200

    version updates, added docker based scripts for deploy and compile

--- src/main/java/org/rapidpm/vaadin/component/login/LoginView.java
+++ src/main/java/org/rapidpm/vaadin/component/login/LoginView.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2018 Sven Ruppert (sven.ruppert@gmail.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -96,6 +96,10 @@ public abstract class LoginView extends Composite<HorizontalLayout> implements H
 
   public String password() {
     return password.getValue();
+  }
+
+  public Boolean rememberMe() {
+    return rememberMe.getValue();
   }
 
   private void validate() {
